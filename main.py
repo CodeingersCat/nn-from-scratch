d7be9ce1092132fa71cf47d42616e3ccd593ba52
Shrawan Baruah <rocketpilot763@gmail.com>
2022-11-07 00:11:53 +0530

    Adding some elegance

--- main.py
+++ main.py
@@ -6,18 +6,18 @@ from test import test_random_image
 k = 2
 
 while(k == 2):
-    data = pd.read_csv('Datasets/train.csv')
+    # Loading training dataset
+    data_train = pd.read_csv('Datasets/train.csv')
+    data_train = np.array(data_train)
 
-    data = np.array(data)
-    m, n = data.shape
-
+    m, n = data_train.shape
     print("TRAINING WITH -")
     print("Number of images : "+str(m))
     print("Number of pixels per image : "+str(n-1))
     print()
 
-    # training data
-    data_train = data.T
+    # Framing training data
+    data_train = data_train.T
     Y_train = data_train[0]
     X_train = data_train[1:n]
     X_train = X_train / 255. # Normalising pixel values
@@ -27,28 +27,29 @@ while(k == 2):
     cycles = int(input("Enter the number of learning cycles\n"))
     W1, b1, W2, b2 = gradient_descent(X_train, Y_train, alpha, cycles)
 
-    #test
+    # Loading test dataset
     test_data = pd.read_csv('Datasets/test.csv')
-
     data = np.array(test_data)
-    m, n = test_data.shape
+
+    #TEST
+    m, n = data.shape
 
     print()
     print("TESTING WITH -")
     print("Number of images : "+str(m))
-    print("Number of pixels per image : "+str(n-1))
+    print("Number of pixels per image : "+str(n))
     print()
 
     k = 1
-    while(k == 1):
-        # testing data
-        data_test = data.T
-        print(data_test.shape)
 
+    # testing data
+    data_test = data.T
+    while(k == 1):
         #TEST
-        index = int(input("Enter a random index from 1 to 27999 \n"))
-        data_test = data_test[:, index, None]
-        X_test = data_test / 255.  # Normalising pixel values
+        index = int(input("Enter select a random image number from 0 to 27999 \n"))
+        print(data_test.shape)
+        current_img = data_test[:, index, None]
+        X_test = current_img / 255.  # Normalising pixel values
         test_random_image(X_test, W1, b1, W2, b2)
         k = int(input("Press 1 to try again, 2 to re-train the neural network or press any other key to exit\n"))
 
